ca71c1c22bf94b41634874480d1bae7165dbff0b
Walter White <mandelbrot@mandelbrotset.local>
2018-10-17 14:28:30 -0400

    add motion and console printing; add comment

--- Code/Advanced/headlessJava/HeadlessTest.java
+++ Code/Advanced/headlessJava/HeadlessTest.java
@@ -1,16 +1,27 @@
 import processing.core.PApplet;
 
+// from:
+// https://stackoverflow.com/questions/38104841/processing-in-java-without-ide
+
 public class HeadlessTest extends PApplet {
+
+	int x, y;
+
 	@Override
 	public void settings() {
 		size(200, 200);
+		x = width / 2;
+		y = height / 2;
+		println("Setup Done");
 	}
 
 	@Override
 	public void draw() {
 		background(0);
 		fill(255, 0, 0);
-		ellipse(100, 100, 100, 100);
+		ellipse(x, y, 100, 100);
+		x++;
+		y++;
 	}
 
 	public static void main(String... args) {
